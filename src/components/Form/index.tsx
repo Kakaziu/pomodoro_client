82bfe2d599b710cc4ae7c3375ec76e08e73446dd
Kauã <kaua.borba03@gmail.com>
2023-06-04 21:37:08 -0300

    feat: form logic

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,20 +1,36 @@
-import { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { Button } from "../Button/styled";
 import { FormTag, Camp } from "./styled";
 import { IForm } from "./protocol";
 import IconType from "../IconType";
 
 const Form: FunctionComponent<IForm> = ({ type, camps }) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function onChangeInput(type: string, e: React.ChangeEvent<HTMLInputElement>) {
+    if (type === "Firstname") setFirstName(e.target.value);
+    if (type === "Lastname") setLastName(e.target.value);
+    if (type === "E-mail") setEmail(e.target.value);
+    if (type === "Password") setPassword(e.target.value);
+  }
+
   return (
     <FormTag>
       <h1>{type === "REGISTER" ? "SIGN UP" : "SIGN IN"}</h1>
       {camps.map((camp) => {
         return (
-          <Camp>
+          <Camp key={camp.type}>
             <span>
               <IconType type={camp.type} />
             </span>
-            <input type={camp.inputType} placeholder={camp.type} />
+            <input
+              type={camp.inputType}
+              placeholder={camp.type}
+              onChange={(e) => onChangeInput(camp.type, e)}
+            />
           </Camp>
         );
       })}
