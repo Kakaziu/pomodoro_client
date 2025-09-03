3f23b13546540c39dc95a4268a18725d623f82e8
Kauã <kaua.borba03@gmail.com>
2025-09-03 01:11:15 -0300

    fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://pomodoro-api-crav-81wbq5c0u-kakazius-projects.vercel.app/",
+  // baseURL: "https://pomodoro-api-crav-81wbq5c0u-kakazius-projects.vercel.app/",
+  baseURL: "http://localhost:3000/",
 });
 
 export default api;
