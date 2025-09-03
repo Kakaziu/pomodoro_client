c6c78fac4e70661056dd4fd6968e66028507a258
Kauã Borba <98915656+Kakaziu@users.noreply.github.com>
2025-09-02 23:51:42 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://pomodoro-790pahmju-kakaziu.vercel.app/",
+  baseURL: "https://pomodoro-api-crav-81wbq5c0u-kakazius-projects.vercel.app/",
 });
 
 export default api;
